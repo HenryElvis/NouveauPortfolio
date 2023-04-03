623ee68fb5e6e7480d13d2a253e252e46a40dca9
Elvis <elvishenry2402@gmail.com>
2023-04-03 23:36:34 +0200

    Terminal proto

--- javascript/index.js
+++ javascript/index.js
@@ -2,3 +2,47 @@
 var age = "5";
 var myName = "Elvis";
 // alert(myName);
+var CMD = /** @class */ (function () {
+    function CMD(inputId, outputId) {
+        this.result = '';
+        this.inputElement = document.getElementById(inputId);
+        this.outputElement = document.getElementById(outputId);
+    }
+    CMD.prototype.showPrompt = function () {
+        this.inputElement.value = "";
+        this.inputElement.focus();
+    };
+    CMD.prototype.runCommand = function (command) {
+        switch (command) {
+            case "help":
+                this.result = "Output:\n" + "Liste des commandes disponibles : \n help - affiche cette aide \n contact - affiche mes informations de contact";
+                this.writeOutput("Liste des commandes disponibles : \n help - affiche cette aide \n contact - affiche mes informations de contact");
+                break;
+            case "contact":
+                this.result = "Output:\n" + "Email :";
+                this.writeOutput("Email : contact@monportfolio.com \n Téléphone : 01 23 45 67 89");
+                break;
+            default:
+                this.result = "Output:\n" + "Commande inconnue : ".concat(command, ". Tapez 'help' pour afficher la liste des commandes disponibles.");
+                this.writeOutput("Commande inconnue : ".concat(command, ". Tapez 'help' pour afficher la liste des commandes disponibles."));
+        }
+    };
+    CMD.prototype.clearCommand = function () {
+        this.inputElement.value = '';
+        this.outputElement.textContent = '';
+        this.result = '';
+    };
+    CMD.prototype.writeOutput = function (message) {
+        var p = document.createElement("p");
+        p.innerText = message;
+        this.outputElement.appendChild(p);
+    };
+    return CMD;
+}());
+var cmd = new CMD("input", "output");
+document.getElementById("submit").addEventListener("click", function () {
+    var command = cmd.inputElement.value;
+    cmd.clearCommand();
+    cmd.runCommand(command);
+    cmd.showPrompt();
+});
--- typescript/index.ts
+++ typescript/index.ts
@@ -1,3 +1,58 @@
 var age:String = "5";
 var myName:String = "Elvis";
 // alert(myName);
+
+class CMD {
+    public inputElement: HTMLInputElement;
+    private outputElement: HTMLDivElement;
+    private result: string = '';
+
+    constructor(inputId: string, outputId: string) {
+      this.inputElement = document.getElementById(inputId) as HTMLInputElement;
+      this.outputElement = document.getElementById(outputId) as HTMLDivElement;
+    }
+    
+    public showPrompt() {
+      this.inputElement.value = "";
+      this.inputElement.focus();
+    }
+    
+    public runCommand(command: string) {
+      switch(command) {
+        case "help":
+            this.result = "Output:\n" + "Liste des commandes disponibles : \n help - affiche cette aide \n contact - affiche mes informations de contact";
+          this.writeOutput("Liste des commandes disponibles : \n help - affiche cette aide \n contact - affiche mes informations de contact");
+          break;
+        case "contact":
+            this.result = "Output:\n" + "Email :";
+          this.writeOutput("Email : contact@monportfolio.com \n Téléphone : 01 23 45 67 89");
+          break;
+        default:
+            this.result = "Output:\n" + `Commande inconnue : ${command}. Tapez 'help' pour afficher la liste des commandes disponibles.`;
+          this.writeOutput(`Commande inconnue : ${command}. Tapez 'help' pour afficher la liste des commandes disponibles.`);
+      }
+    }
+
+    public clearCommand() {
+        this.inputElement.value = '';
+        this.outputElement.textContent = '';
+        this.result = '';
+      }
+    
+    private writeOutput(message: string) {
+      const p = document.createElement("p");
+      p.innerText = message;
+      this.outputElement.appendChild(p);
+    }
+  }
+
+
+const cmd = new CMD("input", "output");
+
+(document.getElementById("submit") as HTMLButtonElement).addEventListener("click", () => {
+    const command = cmd.inputElement.value;
+    cmd.clearCommand();
+    cmd.runCommand(command);
+    cmd.showPrompt();
+  });
+  
